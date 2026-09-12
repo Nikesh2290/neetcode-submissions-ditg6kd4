990f3c61fd66dc4c253276616ccf2587ba9805dd
Nikesh Kumar <79050581+Nikesh2290@users.noreply.github.com>
2026-09-12 11:57:31 +0530

    Add: house-robber-ii - submission-0

---
+++ Data Structures & Algorithms/house-robber-ii/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def solve(self,nums,n,indx,dp,taken):
+        if indx >= n:
+            return 0
+        if dp[indx][taken] != -1:
+            return dp[indx][taken]
+        take = -1
+        if not taken:
+            take = nums[indx] + self.solve(nums,n,indx+1,dp,1)
+        leave = self.solve(nums,n,indx+1,dp,0)
+        dp[indx][taken] = max(take,leave)
+        return dp[indx][taken]
+
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+        dp1 = [[-1,-1] for _ in range(n)]
+        dp2 = [[-1,-1] for _ in range(n)]
+        ans = max(self.solve(nums,n-1,0,dp1,0),self.solve(nums,n,1,dp2,0))
+        return ans
